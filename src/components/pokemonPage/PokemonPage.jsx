a530c3f0d89638a6a14d233f361d6a75eb3d8739
Maik Henrique <maik.hsp.contato@gmail.com>
2020-09-07 20:55:24 -0300

    :heavy_plus_sign: feat - Implemented Basic Structure for the static version of the page

--- src/components/pokemonPage/PokemonPage.jsx
+++ src/components/pokemonPage/PokemonPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StatBar } from "./../index";
 import styles from "./PokemonPage.module.scss";
 
 const PokemonPage = (props) => {
@@ -6,19 +7,19 @@ const PokemonPage = (props) => {
     return (
         <section className={styles.pokePageSection}>
             <div className={styles.pokePageContainer}>
-                <article className={styles.profile}>   
-                    <div className={styles.profileAvatarContainer}> 
-                        <div className={styles.profileAvatar}> 
+                <article className={styles.profile}>
+                    <div className={styles.profileAvatarContainer}>
+                        <div className={styles.profileAvatar}>
                             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg" alt="Pokémon Profile Picture" />
                         </div>
                         <h3>Ditto</h3>
                     </div>
-                    <div className={styles.basicInfoContainer}> 
-                        <div className={styles.basicInfoDescription}> 
+                    <div className={styles.basicInfoContainer}>
+                        <div className={styles.basicInfoDescription}>
                             <h2>Description</h2>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio nostrum voluptate assumenda sint veniam mollitia.</p>
                         </div>
-                        <div className={styles.basicInfoEvolutionChain}> 
+                        <div className={styles.basicInfoEvolutionChain}>
                             <h2>Evolution Chain</h2>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio nostrum voluptate assumenda sint veniam mollitia.</p>
                         </div>
@@ -34,78 +35,76 @@ const PokemonPage = (props) => {
                     </div>
                 </article>
                 <hr />
-                <article className={styles.strengths}> 
-                    <div className={styles.strengthAgainst}> 
-                        <h2>Strong Against</h2>
-                        <p>This pokemon is not naturally strong agaisnt any type</p>
-                    </div>
-                    <div className={styles.strengthAgainst}> 
-                        <h2>Strong Against</h2>
-                        <div className={styles.pokeType}>
-                            <div>
-                                <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
-                                <h4>Normal</h4>
+                <article className={styles.strengths}>
+                    <div className={styles.strengthsContainer}>
+                        <div className={styles.strengthAgainst}>
+                            <h2>Strong Against</h2>
+                            <p>This pokemon is not naturally strong agaisnt any type</p>
+                        </div>
+                        <div className={styles.strengthAgainst}>
+                            <h2>Strong Against</h2>
+                            <div className={styles.pokeType}>
+                                <div>
+                                    <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
+                                    <h4>Normal</h4>
+                                </div>
+                            </div>
+                            <div className={styles.pokeType}>
+                                <div>
+                                    <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
+                                    <h4>Normal</h4>
+                                </div>
+                            </div>
+                            <div className={styles.pokeType}>
+                                <div>
+                                    <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
+                                    <h4>Normal</h4>
+                                </div>
                             </div>
                         </div>
-                        <div className={styles.pokeType}>
-                            <div>
-                                <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
-                                <h4>Normal</h4>
+                        <div className={styles.strengthAgainst}>
+                            <h2>Weak Against</h2>
+                            <div className={styles.pokeType}>
+                                <div>
+                                    <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
+                                    <h4>Normal</h4>
+                                </div>
                             </div>
-                        </div>
-                        <div className={styles.pokeType}>
-                            <div>
-                                <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
-                                <h4>Normal</h4>
+                            <div className={styles.pokeType}>
+                                <div>
+                                    <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
+                                    <h4>Normal</h4>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className={styles.strengthAgainst}> 
-                        <h2>Weak Against</h2>
-                        <div className={styles.pokeType}>
-                            <div>
-                                <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
-                                <h4>Normal</h4>
-                            </div>
-                        </div>
-                        <div className={styles.pokeType}>
-                            <div>
-                                <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
-                                <h4>Normal</h4>
-                            </div>
-                        </div>
-                        <div className={styles.pokeType}>
-                            <div>
-                                <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
-                                <h4>Normal</h4>
+                            <div className={styles.pokeType}>
+                                <div>
+                                    <img src="https://freestencilgallery.com/wp-content/uploads/2016/07/Pokemon-Normal-Type-Symbol-Stencil-thumb.jpg" alt="Type of pokemon: Normal" />
+                                    <h4>Normal</h4>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </article>
                 <hr />
-                <article className={styles.moves}> 
+                <article className={styles.moves}>
                     <h2>Moves</h2>
                     <p>Here there's a list of some moves that the Pokémon can perform or learn to.</p>
-                    <div className={styles.movesMoveContainer}> 
+                    <div className={styles.movesMoveContainer}>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod eaque, neque iure animi qui sunt!</p>
                         <div className={styles.movesMoveStatsBarContainer}>
-                            <div className={styles.movesMoveStatsBar}> 
+                            <div className={styles.movesMoveStatsBar}>
                                 <p>Power</p>
-                                <div>
-                                    <p>Bar</p>
-                                </div>
+                                <StatBar baseStat={100} statName={"power"} />
                             </div>
-                            <div className={styles.movesMoveStatsBar}> 
+                            <div className={styles.movesMoveStatsBar}>
                                 <p>Accuracy</p>
-                                <div>
-                                    <p>Bar</p>
-                                </div>
+                                <StatBar baseStat={100} statName={"accuracy"} />
                             </div>
                         </div>
                     </div>
                 </article>
                 <hr />
-                <article className={styles.additionalInfo}> 
+                <article className={styles.additionalInfo}>
                     <h2>Additional Information</h2>
                     <p>Weight: <span>35</span>kg</p>
                     <p>Height: <span>1</span>m</p>
