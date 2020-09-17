1a72b2dbd0d16ea7fb088799bfebbe886a27084a
Maik Henrique <maik.hsp.contato@gmail.com>
2020-09-17 20:19:40 -0300

    :hammer: fix - Fixed stylesheet issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Header, Main, Footer} from "./components/index";
-import "./App.module.scss";
+import "./App.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
