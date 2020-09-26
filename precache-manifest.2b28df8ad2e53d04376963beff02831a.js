1d475e6a42c3fca8abb30b2bca97c97d6f884952
MaikHenriqueSP <MaikHenriqueSP@users.noreply.github.com>
2020-09-26 23:19:56 +0000

    deploy: cddda9da06c5295fb43e34b1566f18af2e5acad2

---
+++ precache-manifest.2b28df8ad2e53d04376963beff02831a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822b507e5a4551939f032f74ac0cc66c",
+    "url": "/react-poke-app/index.html"
+  },
+  {
+    "revision": "e5493a8f32049d6312b7",
+    "url": "/react-poke-app/static/css/main.8c3adb5d.chunk.css"
+  },
+  {
+    "revision": "4cbb3d568fb841bc230e",
+    "url": "/react-poke-app/static/js/2.32e31ac2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-poke-app/static/js/2.32e31ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5493a8f32049d6312b7",
+    "url": "/react-poke-app/static/js/main.cbe611e1.chunk.js"
+  },
+  {
+    "revision": "6a7d4174b93afe09faa4",
+    "url": "/react-poke-app/static/js/runtime-main.8552900e.js"
+  },
+  {
+    "revision": "14540979a6685ab378bf0cee1e97f942",
+    "url": "/react-poke-app/static/media/bug.14540979.svg"
+  },
+  {
+    "revision": "b7d4f57872507c79b1ecd192aad61357",
+    "url": "/react-poke-app/static/media/dark.b7d4f578.svg"
+  },
+  {
+    "revision": "18a1dbc9e3a69ba77e890268451026e4",
+    "url": "/react-poke-app/static/media/dragon.18a1dbc9.svg"
+  },
+  {
+    "revision": "68b142da814c55f9225db2c16193ba01",
+    "url": "/react-poke-app/static/media/electric.68b142da.svg"
+  },
+  {
+    "revision": "603e08d1937f16395d2d304ef6d30ab7",
+    "url": "/react-poke-app/static/media/fairy.603e08d1.svg"
+  },
+  {
+    "revision": "8ce4fa81f8f29f8c6871b88f78916637",
+    "url": "/react-poke-app/static/media/fighting.8ce4fa81.svg"
+  },
+  {
+    "revision": "33d14b307cd671ab9c1ca76f4788df7a",
+    "url": "/react-poke-app/static/media/fire.33d14b30.svg"
+  },
+  {
+    "revision": "8c1e094fa4fa86cf9a4b943d6b6c9253",
+    "url": "/react-poke-app/static/media/flying.8c1e094f.svg"
+  },
+  {
+    "revision": "ff3973459922626c46b8935bd3517deb",
+    "url": "/react-poke-app/static/media/ghost.ff397345.svg"
+  },
+  {
+    "revision": "d589dd00e175efada7294faf05f52ac6",
+    "url": "/react-poke-app/static/media/grass.d589dd00.svg"
+  },
+  {
+    "revision": "289c4927e264d15f5be41d5c53f5c601",
+    "url": "/react-poke-app/static/media/ground.289c4927.svg"
+  },
+  {
+    "revision": "f82b7e5d85efb43a4e5001ed490269d4",
+    "url": "/react-poke-app/static/media/ice.f82b7e5d.svg"
+  },
+  {
+    "revision": "8f2a7aaa9ae7bd49980f1b2b99a2577f",
+    "url": "/react-poke-app/static/media/normal.8f2a7aaa.svg"
+  },
+  {
+    "revision": "07600455b6331e081ceb27516128e6b7",
+    "url": "/react-poke-app/static/media/pikachu-removebg-preview.07600455.png"
+  },
+  {
+    "revision": "183135e96caba5df8c380e0d2acdea6a",
+    "url": "/react-poke-app/static/media/poison.183135e9.svg"
+  },
+  {
+    "revision": "cc72fe69d9dfc50d4b6f6fadb0bc9fd6",
+    "url": "/react-poke-app/static/media/poke-logo.cc72fe69.jpg"
+  },
+  {
+    "revision": "60982f05e13fbd9b6e6d96ce3456d365",
+    "url": "/react-poke-app/static/media/psychic.60982f05.svg"
+  },
+  {
+    "revision": "499a0fa10362807e9a3218082be3d288",
+    "url": "/react-poke-app/static/media/rock.499a0fa1.svg"
+  },
+  {
+    "revision": "95342da3d89b3644b12d596a41d631c7",
+    "url": "/react-poke-app/static/media/steel.95342da3.svg"
+  },
+  {
+    "revision": "e29fb8e379f9ed5335a02eb3881d25d6",
+    "url": "/react-poke-app/static/media/water.e29fb8e3.svg"
+  }
+]);
