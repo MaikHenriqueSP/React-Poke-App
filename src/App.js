cdc944127840914fddc6c545e25975b1e131e45e
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2020-09-26 20:00:10 -0300

    add basename to route to avoid routing issues

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
-import { Header, Main, Footer} from "./components/index";
+import { Header, Main, Footer } from "./components/index";
 import "./App.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
-
-    render() {
-        return (
-            <>
-                <Router>
-                    <Header />
-                    <Main />
-                    <Footer />
-                </Router>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Router basename={process.env.PUBLIC_URL}>
+          <Header />
+          <Main />
+          <Footer />
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
